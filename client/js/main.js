a56ebf506754557e23a721e19eaa837ccc2f4105
elvece <lvcifferello@gmail.com>
2015-11-03 12:17:50 -0700

    starting client side to get MVP and then refactor into backbone, render items from get route initialization functioning

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,23 @@
 // add scripts
 
 $(document).on('ready', function() {
-  console.log('sanity check!');
+  renderItems();
+  $('#message').hide();
 });
+
+
+//helper function to render todo items
+function renderItems(){
+  $.get('/api/todos', function(data){
+    for (var i = 0; i < data.length; i++) {
+      $('#all-todos').append(
+        '<tr>'+
+          '<td>'+data[i].title+'</td>'+
+          '<td>'+data[i].completed+'</td>'+
+          '<td><a class="btn btn-primary btn-xs edit-button" data-toggle="modal" data-target="#edit-modal" id="'+data[i]._id+'" role="button">Edit</a>'+
+          '&nbsp;<a class="btn btn-danger btn-xs delete-button" data-toggle="modal" data-target="#delete-modal" id="'+data[i]._id+'" role="button">Delete</a></td'+
+        '</tr>'
+      );
+    }
+  });
+}
