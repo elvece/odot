51c72671734d8ffadb499faca4681cd2fc1d42ac
elvece <lvcifferello@gmail.com>
2015-11-03 11:31:12 -0700

    added post route, changed route names

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var ToDo = mongoose.model('todo');
 
 
-router.get('/list', function(req, res, next) {
+router.get('/todos', function(req, res, next) {
   ToDo.find(function(err, data){
     if(err){
       throw err;
@@ -13,5 +13,22 @@ router.get('/list', function(req, res, next) {
     }
   });
 });
+
+router.post('/todos', function(req, res, next){
+  var newToDo = new ToDo({
+    title: req.body.title,
+    completed: false
+  });
+
+  newToDo.save(function(err, data){
+    if (err){
+      throw err;
+    } else {
+      res.json({Message:'To do item saved!'});
+    }
+  });
+});
+
+
 
 module.exports = router;
