5cf2206b104383de73ae31ee66c2f29602b47dac
elvece <lvcifferello@gmail.com>
2015-11-04 14:45:09 -0700

    changed and added markup to todo view, added events, and comments for event execution

--- client/js/views/TodoView.js
+++ client/js/views/TodoView.js
@@ -1,19 +1,41 @@
-//calling app?
-var app =  app || {};
+var TodoView = Backbone.View.extend({
 
-//To do item view
-app.TodoView = Backbone.View.extend({
+  tagName:  'li',
 
-  //sets module name to js file
-  moduleName: '{TodoView.js}',
-//route path of api url
-//   urlRoot:
-//sets template for a single item
-//   template:
-//corresponds to DOM events for a specific to do item
-//   events:
-//initalizes module, gets title, completed, status, delete and show item
-//  initalize:
-//renders the titles of to do item
-//  render:
+  //cache the template function for a single item.
+  todoTpl: _.template( $('#item-template').html() ),
+
+  events: {
+    'dblclick label': 'edit',
+    'keypress .edit': 'updateOnEnter',
+    'blur .edit':   'close'
+  },
+
+  //called when the view is first created
+  initialize: function(options) {
+    this.$el = $('#todo');
+    //to access passed options in your view
+    this.options = options || {};
+  },
+
+  //renders to do item titles
+  render: function() {
+    //$el = reference to jQuery object
+    //todoTpl = ref to underscore template
+    //replaces html of dom element with the instantiated template containing the model's attributes
+    this.$el.html(this.todoTpl( this.model.attributes ));
+    this.input = this.$('.edit');
+    return this;
+  },
+
+  edit: function() {
+    //executed when todo label is double clicked
+  },
+
+  close: function() {
+    //executed when todo loses focus
+  }
 });
+
+//create a view for a todo
+var todoView = new TodoView({model: todo});
