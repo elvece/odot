eae71bc30fc00759f2412fe24825d9f09efedd60
elvece <lvcifferello@gmail.com>
2015-11-04 22:36:41 -0700

    events and corresponding methods to to do view, set enter key to variable and called new app view, uncommented file paths in index

--- client/js/app.js
+++ client/js/app.js
@@ -1,9 +1,12 @@
-
-var app = app || {};
+//console keeps yelling at me whenever app is instantiated, why?
+// var app = app || {};
 // console.log(app)
+
+//sets variable to key code of enter key
+var ENTER_KEY = 13;
 
 //application initiate
 $(function() {
   //creating app
-  new app.AppView();
+  new AppView();
 });
--- client/js/views/TodoView.js
+++ client/js/views/TodoView.js
@@ -1,14 +1,18 @@
+//instances of this view will be associated with each individual to do record
+//todo instances can handle editing, updating, and destroying their associated todo.
 
 //*** TO DO VIEW ***//
 var TodoView = Backbone.View.extend({
 
   //creates view element, defaults to div
-  tagName:  'li',
+  tagName: 'li',
   //sets class name of this element
   className: '',
 
-  //cache the template function for a single item.
-  todoTpl: _.template($('#item-template').html()),
+  //cache the template function for a single item
+  todoTemplate: _.template($('#item-template').html()),
+
+  //DOM events specific to an item
   //jQuery’s .delegate() underneath - don’t have to worry about whether a particular element has been rendered to the DOM yet or not
   events: {
     'click .toggle': 'toggleCompleted',
@@ -18,40 +22,60 @@ var TodoView = Backbone.View.extend({
     'blur .edit': 'close'
   },
 
-  //called when the view is first created
+  //TodoView listens for changes to its model, then re-renders
+  //because 1-1 correspondence between a todo and a a ToDoView, direct reference on the model is set
   initialize: function(options) {
     //binds a function to an object so that anytime the function is called the value of this will be the object
-    this.model.bind('change', _.bind(this.render, this));
-    this.$el = $('#todo');
+    // this.model.bind('change', _.bind(this.render, this));
+    // this.$el = $('#todo');
     //to access passed options in your view
-    this.options = options || {};
+    // this.options = options || {};
+
+    this.listenTo(this.model, 'change', this.render);
   },
 
   //render defines the logic for rendering a template
   //renders to do item titles
   render: function() {
     //$el = reference to jQuery object
-    //todoTpl = ref to underscore template
+    //todoTemplate = ref to underscore template
     //replaces html of dom element with the instantiated template containing the model's attributes
-    this.$el.html(this.todoTpl(this.model.attributes));
-    //what is this doing
-    this.input = this.$('.edit');
+    this.$el.html(this.todoTemplate(this.model.attributes));
+    //caching the input element within the instantiated template
+    this.$input = this.$('.edit');
     //convention to return this, as it makes views reusable in parent views and renders all elements once the entire list is populated
     return this;
   },
 
+  //activated when todo label is double clicked
   edit: function() {
-    //executed when todo label is double clicked
+    //sets element class to editing for tracking
+    this.$el.addClass('editing');
+    //displays input field
+    this.$input.focus();
   },
 
+  //closes editing mode, saves model changes
   close: function() {
-    //executed when todo loses focus
+    //changed value
+    var value = this.$input.val().trim();
+    //if there is a value
+    if (value) {
+      //save it as the title attribute of the model
+      this.model.save({title: value});
+    }
+    //remove editing class to deactive tracking
+    this.$el.removeClass('editing');
   },
 
-  updateOnEnter: function(e) {
-  // executed on each keypress when in todo edit mode,
+  //checks if enter key press and executes close function above
+  updateOnEnter: function(event) {
+    if (event.which === ENTER_KEY) {
+      //closes edit input
+      this.close();
+    }
   }
 });
 
-//create a view for a todo
-var todoView = new TodoView({model: todo});
+//to create a view for a todo
+//var todoView = new TodoView({model: todo});
