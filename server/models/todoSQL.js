ad2cc0761b7bae32b8903b6af1371551fa33fb7c
elvece <lvcifferello@gmail.com>
2015-11-08 15:47:10 -0700

    added version tag

--- server/models/todoSQL.js
+++ server/models/todoSQL.js
@@ -1,8 +1,8 @@
-var pg = require('pg');
-var path = require('path');
-var connectionString = require(path.join(__dirname, '../', '../', 'config'));
+// var pg = require('pg');
+// var path = require('path');
+// var connectionString = require(path.join(__dirname, '../', '../', 'config'));
 
-var client = new pg.Client(connectionString);
-client.connect();
-var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, title VARCHAR(40) not null, completed BOOLEAN)');
-query.on('end', function() { client.end(); });
+// var client = new pg.Client(connectionString);
+// client.connect();
+// var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, title VARCHAR(40) not null, completed BOOLEAN)');
+// query.on('end', function() { client.end(); });
