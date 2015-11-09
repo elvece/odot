95dd312d8d95f85854a4ba9d458034d6fe5a8839
elvece <lvcifferello@gmail.com>
2015-11-08 17:00:33 -0700

    added delete route with postgresql, works with httpie testing; fixed post type so compelted reads false:

--- server/routes/api_sql.js
+++ server/routes/api_sql.js
@@ -19,7 +19,7 @@ router.post('/todos', function(req, res){
       return res.status(500).json({success: false, data: err});
     }
 
-    client.query("INSERT INTO items(title, completed) values($1, $2)", [data.title, data.complete]);
+    client.query("INSERT INTO items(title, completed) values($1, $2)", [data.title, data.completed]);
 
     var query = client.query("SELECT * FROM items ORDER BY id ASC");
 
@@ -63,26 +63,53 @@ router.put('/todos/:id', function(req, res) {
   var data = {title: req.body.title, completed: req.body.completed};
 
   pg.connect(connectionString, function(err, client, done) {
-      if(err) {
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).send(json({ success: false, data: err}));
+    }
+
+    client.query("UPDATE items SET title=($1), completed=($2) WHERE id=($3)", [data.title, data.completed, id]);
+
+    var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+    query.on('row', function(row) {
+        results.push(row);
+    });
+
+    query.on('end', function() {
         done();
-        console.log(err);
-        return res.status(500).send(json({ success: false, data: err}));
-      }
-
-      client.query("UPDATE items SET title=($1), completed=($2) WHERE id=($3)", [data.title, data.completed, id]);
-
-      var query = client.query("SELECT * FROM items ORDER BY id ASC");
-
-      query.on('row', function(row) {
-          results.push(row);
-      });
-
-      query.on('end', function() {
-          done();
-          return res.json(results);
-      });
+        return res.json(results);
+    });
   });
+});
 
+
+router.delete('/todos/:id', function(req, res) {
+
+  var results = [];
+  var id = req.params.id;
+
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({ success: false, data: err});
+    }
+
+    client.query("DELETE FROM items WHERE id=($1)", [id]);
+
+    var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+    query.on('row', function(row) {
+        results.push(row);
+    });
+
+    query.on('end', function() {
+        done();
+        return res.json(results);
+    });
+  });
 });
 
 module.exports = router;
